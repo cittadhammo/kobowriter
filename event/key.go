1e98c5a16bf4cd6fa521ba02dbc1d7e17487f746
Cittadhammo <82962905+cittadhammo@users.noreply.github.com>
2024-01-20 17:01:15 +0000

    new one

--- event/key.go
+++ event/key.go
@@ -1,4 +1,4 @@
-//AZERTYkeybindings
+//CSAkeybindings
 
 package event
 
@@ -6,63 +6,64 @@ var KeyCode = map[int]string{
 	0: "KEY_RESERVED",
 	1: "KEY_ESC",
 
-	2:  "&",
-	3:  "é",
-	4:  "\"",
-	5:  "'",
-	6:  "(",
-	7:  "-",
-	8:  "è",
-	9:  "_",
-	10: "ç",
-	11: "à",
-	12: ")",
-	13: "=",
+	2:  "1", // CSA: "&" is replaced with "1"
+	3:  "2", // CSA: "é" is replaced with "2"
+	4:  "3", // CSA: "\"" is replaced with "3"
+	5:  "4", // CSA: "'" is replaced with "4"
+	6:  "5", // CSA: "(" is replaced with "5"
+	7:  "6", // CSA: "-" is replaced with "6"
+	8:  "7", // CSA: "è" is replaced with "7"
+	9:  "8", // CSA: "_" is replaced with "8"
+	10: "9", // CSA: "ç" is replaced with "9"
+	11: "0", // CSA: "à" is replaced with "0"
+	12: "-", // CSA: ")" is replaced with "-"
+	13: "=", // CSA: "=" stays "="
 
 	14: "KEY_BACKSPACE",
 	15: "KEY_TAB",
 
-	16: "a",
-	17: "z",
-	18: "e",
-	19: "r",
-	20: "t",
-	21: "y",
-	22: "u",
-	23: "i",
-	24: "o",
-	25: "p",
-	26: "^",
-	27: "$",
+	16: "q", // CSA: "a" is replaced with "q"
+	17: "w", // CSA: "z" is replaced with "w"
+	18: "e", // CSA: "e" stays "e"
+	19: "r", // CSA: "r" stays "r"
+	20: "t", // CSA: "t" stays "t"
+	21: "y", // CSA: "y" stays "y"
+	22: "u", // CSA: "u" stays "u"
+	23: "i", // CSA: "i" stays "i"
+	24: "o", // CSA: "o" stays "o"
+	25: "p", // CSA: "p" stays "p"
+	26: "^", // CSA: "^" is replaced with "["
+	27: "ç", // CSA: "$" is replaced with "]"
 	28: "KEY_ENTER",
 	29: "KEY_L_CTRL",
 
-	30: "q",
-	31: "s",
-	32: "d",
-	33: "f",
-	34: "g",
-	35: "h",
-	36: "j",
-	37: "k",
-	38: "l",
-	39: "m",
-	40: "ù",
-	41: "*",
+	30: "a", // CSA: "q" is replaced with "a"
+	31: "s", // CSA: "s" stays "s"
+	32: "d", // CSA: "d" stays "d"
+	33: "f", // CSA: "f" stays "f"
+	34: "g", // CSA: "g" stays "g"
+	35: "h", // CSA: "h" stays "h"
+	36: "j", // CSA: "j" stays "j"
+	37: "k", // CSA: "k" stays "k"
+	38: "l", // CSA: "l" stays "l"
+	39: ";", // CSA: "m" is replaced with ";"
+	40: "è", // CSA: "ù" is replaced with "'"
+	41: "ù", // CSA: "*" is replaced with "`" // seems to be dead on hp keyboard
+	
+	// 41 seems to be reverse with 43 on my hp keyboard
 
 	42: "KEY_L_SHIFT",
-	43: "<",
-	44: "w",
-	45: "x",
-	46: "c",
-	47: "v",
-	48: "b",
-	49: "n",
-	50: ",",
-	51: ";",
-	52: ":",
-	53: "!",
-	54: "KEY_R_SHIFT",
+	43: "à", // CSA: "<" is replaced with "\"
+	44: "z", // CSA: "w" is replaced with "z"
+	45: "x", // CSA: "x" stays "x"
+	46: "c", // CSA: "c" stays "c"
+	47: "v", // CSA: "v" stays "v"
+	48: "b", // CSA: "b" stays "b"
+	49: "n", // CSA: "n" stays "n"
+	50: "m", // CSA: "," stays ","
+	51: ",", // CSA: ";" is replaced with "."
+	52: ".", // CSA: ":" is replaced with "/"
+	53: "é", // CSA: "!" is replaced with "KEY_R_SHIFT"
 
 	55: "KEY_KPASTERISK",
 	56: "KEY_L_ALT",
@@ -107,21 +108,21 @@ var KeyCode = map[int]string{
 }
 
 var KeyCodeMaj = map[int]string{
-	2:  "1",
-	3:  "2",
-	4:  "3",
-	5:  "4",
-	6:  "5",
-	7:  "6",
-	8:  "7",
-	9:  "8",
-	10: "9",
-	11: "0",
-	12: "°",
+	2:  "!",
+	3:  "\"",
+	4:  "#",
+	5:  "$",
+	6:  "%",
+	7:  "?",
+	8:  "&",
+	9:  "*",
+	10: "(",
+	11: ")",
+	12: "_",
 	13: "+",
 
-	16: "A",
-	17: "Z",
+	16: "Q",
+	17: "W",
 	18: "E",
 	19: "R",
 	20: "T",
@@ -131,9 +132,9 @@ var KeyCodeMaj = map[int]string{
 	24: "O",
 	25: "P",
 	26: "¨",
-	27: "£",
+	27: "Ç",
 
-	30: "Q",
+	30: "A",
 	31: "S",
 	32: "D",
 	33: "F",
@@ -142,70 +143,131 @@ var KeyCodeMaj = map[int]string{
 	36: "J",
 	37: "K",
 	38: "L",
-	39: "M",
-	40: "%",
-	41: "µ",
+	39: ":",
+	40: "È",
+	41: "Ù",
 
-	43: ">",
-	44: "W",
+	43: "À",
+	44: "Z",
 	45: "X",
 	46: "C",
 	47: "V",
 	48: "B",
 	49: "N",
-	50: "?",
-	51: ".",
-	52: "/",
-	53: "§",
+	50: "M",
+	51: "'",
+	52: ".",
+	53: "É",
+}
+
+var KeyCodeAlt = map[int]string{
+	1:  "\\",
+	2:  "¹",
+	3:  "@",
+	4:  "³",
+	5:  "¼",
+	6:  "½",
+	7:  "¾",
+	8:  "{",
+	9:  "[",
+	10: "]",
+	11: "}",
+	12: "|",
+	13: "¸",
+
+	16: "q",
+	17: "w",
+	18: "e",
+	19: "¶",
+	20: "t",
+	21: "¥",
+	22: "u",
+	23: "i",
+	24: "ø",
+	25: "þ",
+	26: "°",
+	27: "",
+	
+	30: "æ", // CSA: "q" is replaced with "a"
+	31: "ß", // CSA: "s" stays "s"
+	32: "ð", // CSA: "d" stays "d"
+	33: "ª", // CSA: "f" stays "f"
+	34: "g", // CSA: "g" stays "g"
+	35: "h", // CSA: "h" stays "h"
+	36: "j", // CSA: "j" stays "j"
+	37: "k", // CSA: "k" stays "k"
+	38: "l", // CSA: "l" stays "l"
+	39: "´", // CSA: "m" is replaced with ";"
+	40: "{", // CSA: "ù" is replaced with "'"
+	41: "¬", // CSA: "*" is replaced with "`"
+
+
+	43: "`", // CSA: "<" is replaced with "\"
+	44: "«", // CSA: "w" is replaced with "z"
+	45: "»", // CSA: "x" stays "x"
+	46: "¢", // CSA: "c" stays "c"
+	47: "v", // CSA: "v" stays "v"
+	48: "b", // CSA: "b" stays "b"
+	49: "n", // CSA: "n" stays "n"
+	50: "µ", // CSA: "," stays ","
+	51: "<", // CSA: ";" is replaced with "."
+	52: ">", // CSA: ":" is replaced with "/"
+	53: "//", // CSA: "!" is replaced with "KEY_R_SHIFT"
+
 }
 
 var KeyCodeAltGr = map[int]string{
-	3:  "~",
-	4:  "#",
-	5:  "{",
-	6:  "[",
-	7:  "|",
-	8:  "`",
-	9:  "\\",
-	10: "^",
-	11: "@",
-	12: "]",
-	13: "}",
+	1:  "\\",
+	2:  "¹",
+	3:  "@",
+	4:  "³",
+	5:  "¼",
+	6:  "½",
+	7:  "¾",
+	8:  "{",
+	9:  "[",
+	10: "]",
+	11: "}",
+	12: "|",
+	13: "¸",
 
-	16: "æ",
-	17: "«",
-	18: "€",
+	16: "q",
+	17: "w",
+	18: "e",
 	19: "¶",
-	20: "ŧ",
-	21: "←",
-	22: "↓",
-	23: "→",
+	20: "t",
+	21: "¥",
+	22: "u",
+	23: "i",
 	24: "ø",
 	25: "þ",
-	26: "¨",
-	27: "¤",
+	26: "°",
+	27: "",
+	
+	30: "æ", // CSA: "q" is replaced with "a"
+	31: "ß", // CSA: "s" stays "s"
+	32: "ð", // CSA: "d" stays "d"
+	33: "ª", // CSA: "f" stays "f"
+	34: "g", // CSA: "g" stays "g"
+	35: "h", // CSA: "h" stays "h"
+	36: "j", // CSA: "j" stays "j"
+	37: "k", // CSA: "k" stays "k"
+	38: "l", // CSA: "l" stays "l"
+	39: "´", // CSA: "m" is replaced with ";"
+	40: "{", // CSA: "ù" is replaced with "'"
+	41: "¬", // CSA: "*" is replaced with "`"
 
-	30: "@",
-	31: "ß",
-	32: "ð",
-	33: "đ",
-	34: "ŋ",
-	35: "ħ",
 
-	37: "ĸ",
-	38: "ł",
-	39: "µ",
+	43: "`", // CSA: "<" is replaced with "\"
+	44: "«", // CSA: "w" is replaced with "z"
+	45: "»", // CSA: "x" stays "x"
+	46: "¢", // CSA: "c" stays "c"
+	47: "v", // CSA: "v" stays "v"
+	48: "b", // CSA: "b" stays "b"
+	49: "n", // CSA: "n" stays "n"
+	50: "µ", // CSA: "," stays ","
+	51: "<", // CSA: ";" is replaced with "."
+	52: ">", // CSA: ":" is replaced with "/"
+	53: "//", // CSA: "!" is replaced with "KEY_R_SHIFT"
 
-	41: "`",
-
-	43: "|",
-	44: "ł",
-	45: "»",
-	46: "¢",
-	47: "“",
-	48: "”",
-	49: "n",
-	50: "´",
-	51: "─",
-	52: "·",
 }
